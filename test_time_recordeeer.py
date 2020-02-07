a83337f6af610043be336255afe078c1b9fd7249
EndoHizumi <endohizumi0114@gmail.com>
2020-02-07 14:24:38 +0900

    update: 日付を指定して勤怠データを取得できる

--- test_time_recordeeer.py
+++ test_time_recordeeer.py
@@ -26,3 +26,24 @@ class TestLoad(TestCase):
         actual_data = recorder.load(filePath="temp.csv")
         self.assertEqual(actual_data, self.expect_data)
 
+
+class TestFind(TestCase):
+    def setUp(self):
+        self.test_data = [
+            'date,time,state',
+            '2020/01/31,09:10:51,begin',
+            '2020/01/31,09:11:58,login',
+            '2020/01/31,17:55:42,logout',
+            '2020/01/31,17:55:46,fin',
+            '2020/02/03,09:41:19,begin',
+            '2020/02/03,09:41:48,login',
+            '2020/02/03,18:53:49,logout',
+            '2020/02/03,18:53:54,fin',
+        ]
+        self.test_data = list(csv.DictReader(self.test_data))
+
+    def test_指定した日付の勤怠データを取得できる(self):
+        expect_data = ["09:10:51", "09:11:58", "17:55:42", "17:55:46"]
+        actual_data = recorder.find(self.test_data, "2020/01/31")
+        self.assertEqual(actual_data, expect_data)
+
