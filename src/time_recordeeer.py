618b8941a9e922aa1f4bda207b53008604609af1
skikuchi <skikuchi@iij.ad.jp>
2020-07-21 16:54:51 +0900

    update: able to embossing

--- src/time_recordeeer.py
+++ src/time_recordeeer.py
@@ -1,6 +1,7 @@
 import csv
 from io import StringIO
 from typing import Dict, Iterable
+from src.freee import freee
 
 
 def load(in_memory_buffer: StringIO, filePath: str = "") -> Iterable[Dict[str, str]]:
@@ -19,8 +20,12 @@ def find(attendance_data: Iterable[Dict[str, str]], target_date: str) -> Iterabl
     return target_date_attendance
 
 
-def Emboss(time, state):
+def emboss(state):
     # company_idとemp_idを取得する。
-
+    api = freee()
+    emp_info = api.me()['companies'][0]
+    company_id = emp_info['id']
+    employee_id = emp_info['employee_id']
     # タイムレコーダー登録APIを叩く
-    pass
+    res = api.register_time_clocks(company_id, employee_id, state)
+    return res
